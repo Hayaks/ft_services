dd53fa706af7679c51a206fbda2a43001071c858
hayaks <julien.sag36@gmail.com>
2020-08-13 12:57:02 +0200

    wesh

--- setup.sh
+++ setup.sh
@@ -3,15 +3,18 @@
 #                                                         :::      ::::::::    #
 #    setup.sh                                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: jsaguez <marvin@42.fr>                     +#+  +:+       +#+         #
+#    By: user42 <user42@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/08/12 16:55:22 by jsaguez           #+#    #+#              #
-#    Updated: 2020/08/12 17:03:24 by jsaguez          ###   ########.fr        #
+#    Updated: 2020/08/13 12:13:04 by user42           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+:> errlog.txt
+:> log.log
+
 echo "Minikube starting..."
-minikube start --cpus=2 --memory=4096 --disk-size=8000mb --vm-driver=docker
+minikube start --cpus=2 --memory=3000 --disk-size=8000mb --vm-driver=docker
 minikube addons enable metrics-server
-minikube addons enable metallb && sleep 2 && kubectl apply -f ./srcs/metallb.yaml
+minikube addons enable metallb >> log.log 2>>errlog.txt && sleep 2 && kubectl apply -f ./srcs/metallb.yaml >> log.log 2>>errlog.txt
 minikube addons enable dashboard
