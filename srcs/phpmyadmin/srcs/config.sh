8de12faff59b50deeb02a9aa62c80dfc256dc5c2
hayaks <julien.sag36@gmail.com>
2021-03-10 11:23:24 +0100

    Ca marche!

--- srcs/phpmyadmin/srcs/config.sh
+++ srcs/phpmyadmin/srcs/config.sh
@@ -1,6 +1,5 @@
 mv /root/srcs/telegraf.conf /etc/telegraf.conf
-mv /var/www/phpMyAdmin-5.0.2-all-languages ./var/www/phpmyadmin
-mv ./root/srcs/config.inc.php /var/www/phpmyadmin
+mv ./root/srcs/config.inc.php /var/www/phpmyadmin/
 mv ./root/srcs/default.conf /etc/nginx/conf.d/default.conf
 
 openrc
